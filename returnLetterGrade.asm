fe2380181449eed6317ee2a67c9d89ea43413e6f
Michael <mwu250@gmail.com>
2024-11-12 13:46:08 -0800

    feat: task1

--- returnLetterGrade.asm
+++ returnLetterGrade.asm
@@ -2,7 +2,7 @@
 # 11/9/2024
 # Program 2: Practice With Conditionals and Loops
 # Brandon Tseng, Michael Wu, Jonathan Dang
-# Github Repository
+# Github Repository: https://github.com/MIPS-PIPS/Program-2-Practice-With-Conditionals-and-Loops/tree/main
 
 # return letter grade from user input
 # user menu, main label, looping label, exit label
@@ -10,22 +10,23 @@
 # invalid input handling
 
 .macro returnLetterGrade(%decimal)
-	li $t0, %decimal # i think this parts wrong its not loading in properly
+
+	move $t0, %decimal
+				
 	li $a1, 90
 	bge $t0, $a1, gradeA #if score >= 90 its an A
 	
 	li $a1, 80
-	bge $t0, $a1, gradeB
+	bge $t0, $a1, gradeB #if score >= 80 its a B
 	
 	li $a1, 70
-	bge $t0, $a1, gradeC
+	bge $t0, $a1, gradeC #if score >= 70 its a C
 	
 	li $a1, 60
-	bge $t0, $a1, gradeD
+	bge $t0, $a1, gradeD #if score >= 60 its a D
 	
-	j gradeF
-		
-
+	j gradeF #if score >= 0 its a F
+	
 gradeA:
 	li $a0, 'A'
 	j displayGrade
@@ -48,46 +49,98 @@ displayGrade:
 	syscall
 .end_macro
 
+.macro exit
+	li $v0, 4
+	la $a0, exitMessage
+	syscall
+	
+    	li $v0, 10
+    	syscall
+.end_macro 
 
 .data
-	menuPrompt: .asciiz "---------------MAIN MENU---------------\n(1) Get Letter Grade\n(2) Exit Program"
-	enterScore: .asciiz "Please enter a score as in integer value: "
-	enterSelection: .asciiz "Enter '1' or '2' for your selection: "
-	reEnterScore: .asciiz "Would you like to enter a new score?\n (Y)Yes	(N)No\nEnter 'Y' or 'N' for your selection: "
+	menuPrompt: .asciiz "\n---------------MAIN MENU---------------\n(1) Get Letter Grade\n(2) Exit Program\n\nEnter '1' or '2' for your selection: "
+	enterScore: .asciiz "\n---------------------------------------\n\nPlease enter a score as in integer value: "
 	scoreOutput: .asciiz "The grade is: "
+	reEnterScore: .asciiz "\n---------------------------------------\n\nWould you like to enter a new score?\n(Y)Yes	(N)No\n\nEnter 'Y' or 'N' for your selection: "
+	invalidInput: .asciiz "\nPlease enter a valid input\n"
+	exitMessage: .asciiz "\nThe progam will now exit."
+	newline: .asciiz "\n\n"
 .text
 main:
+
+	j loop
+
+invalidMenuChoice:
+	li $v0, 4
+	la $a0, invalidInput
+	syscall
+
+loop:
 	#print menu and get menu selection
+    li $v0, 4
+    la $a0, menuPrompt
+    syscall
+    
+	# get menu selection
+    li $v0, 5
+    syscall
+    move $s0, $v0
+    	
+	beq $s0, 1, getLetterGrade #get letter grade if input == 1
+    beq $s0, 2, exitProgram #exit if input == 2
+    j invalidMenuChoice
+    
+invalidScore:
 	li $v0, 4
-	la $a0, menuPrompt
+	la $a0, invalidInput
 	syscall
 	
-	li $v0, 5
-	syscall
-	move $s0, $v0
-	#prompt for score
+getLetterGrade:
+    #prompt for score
 	li $v0, 4
-	la $a0, enterSelection
+	la $a0, enterScore
 	syscall
-	move $s1, $v0
+    	
+    # get score
+    li $v0, 5
+    syscall
+    move $s1, $v0
+    	
+    bgt $s1, 100, invalidScore
+    blt $s1, 0, invalidScore 
 	
+	# print result
+	li $v0, 4
+	la $a0, scoreOutput
+	syscall
 	#return score as a letter grade
-	returnLetterGrade(s1)
+	returnLetterGrade($s1)
 	
+reEnterScorePrompt:
 	#reenter score prompt
 	li $v0, 4
 	la $a0, reEnterScore
 	syscall
 	
-	li $v0, 5
+	# get if user wants to reenter score
+	li $v0, 12
 	syscall
 	move $s1, $v0
 	
+	beq $s1, 'Y', getLetterGrade
+	beq $s1, 'N', loop
+	
+	# newline
 	li $v0, 4
-	la $a0, reEnterScore
+	la $a0, newline
 	syscall
-loop:
+	
+	#invalid input
+	li $v0, 4
+	la $a0, invalidInput
+	syscall
+	j reEnterScorePrompt
 
-exit:
-	li $v0, 10
-	syscall
+exitProgram:
+	exit
